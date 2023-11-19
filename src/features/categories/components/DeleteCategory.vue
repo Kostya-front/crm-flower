829754c63de477203e5977722ea38de7306c4767
kostya-front <knstntnpasecnik@gmail.com>
2023-11-19 17:46:13 +0600

    create danger-button-component

--- src/features/categories/components/DeleteCategory.vue
+++ src/features/categories/components/DeleteCategory.vue
@@ -1,9 +1,10 @@
 <template>
-  <button @click="onClick(id)" class="btn btn-danger">X</button>
+  <DangerButton @click="onClick(id)" text="X"/>
 </template>
 
 <script setup lang="ts">
 import { useCategoriesStore } from '../store/categoriesStore';
+import DangerButton from '@/shared/ui/DangerButton/DangetButton.vue'
 
 const props = defineProps(['id'])
 const categoriesStore = useCategoriesStore()
---
+++ src/shared/ui/DangerButton/DangetButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="btn btn-danger">{{text}}</button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps(['text'])
+
+
+</script>
+
+<style>
+
+</style>
